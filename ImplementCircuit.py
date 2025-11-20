c2ce84f4a222a846f5462667a4af50807cdc9faa
shweta0312jain-pranjal <shweta0312jain@gmail.com>
2025-11-20 16:57:25 +0530

    Add files via upload

---
+++ ImplementCircuit.py
@@ -0,0 +1,17 @@
+
+a = 12   
+b = 5    
+
+and_output = a & b
+or_output = a | b
+xor_output = a ^ b
+not_a = ~a & 0xFF    
+left_shift = a << 1
+right_shift = b >> 1
+
+print("AND:", and_output)
+print("OR:", or_output)
+print("XOR:", xor_output)
+print("NOT a:", not_a)
+print("a << 1:", left_shift)
+print("b >> 1:", right_shift)
